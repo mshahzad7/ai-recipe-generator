49854e5968b40d9b6d781a2cabd2cfb7f0bb2bb3
mshahzad7 <mshahzad007@hotmail.com>
2025-01-23 02:16:49 -0500

    connect to bedrock

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/* ****************************************************************************************************************
+The code will use the Amplify Authenticator component to scaffold out an entire user authentication flow allowing 
+users to sign up, sign in, reset their password, and confirm sign-in for multifactor authentication (MFA).
+**************************************************************************************************************** */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
